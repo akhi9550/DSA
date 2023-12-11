735105d975c75c1f0c79154a7b03e31e415e5ea2
akhi9550 <akhilc248@gmail.com>
2023-12-11 16:36:14 +0530

    Sorting

---
+++ Sort/InsertionSort/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func InsertionSort(arr []int, n int) []int {
+	for i := 1; i < n; i++ {
+		temp := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] >= temp {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = temp
+	}
+	return arr
+}
+
+func main() {
+	arr := []int{2, 43, 32, 54, 3, 1, 98}
+	n := len(arr)
+	fmt.Println("Unsorted Array is :", arr)
+	result := InsertionSort(arr, n)
+	fmt.Println("Sorted Array is :", result)
+}
