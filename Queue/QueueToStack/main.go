d2ecdb86c433ea415ef433013ab73bfa6f0c29b7
akhi9550 <akhilc248@gmail.com>
2023-12-13 18:15:19 +0530

    Queue

---
+++ Queue/QueueToStack/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func QueneToStack(queue []int) map[int]int {
+	stack := make(map[int]int)
+	for i, v := range queue {
+		stack[i] = v
+	}
+	return stack
+}
+func main() {
+	queue := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println("the quene:", queue)
+	stack := QueneToStack(queue)
+	fmt.Println("the stack", stack)
+}
