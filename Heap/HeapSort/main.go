20f54b18e52574cd5b08f7e82754c52fba1774d5
akhi9550 <akhilc248@gmail.com>
2024-01-03 13:04:38 +0530

    HeapSort

--- Heap/HeapSort/main.go
+++ Heap/HeapSort/main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{12, 11, 13, 4, 5, 7}
+	arr := []int{12, 11, 13, 4, 5, 7, 9}
 	fmt.Println("Unsoted Array :-", arr)
-	heapSort(arr)
-	fmt.Println("Sorted Array:-", arr)
-}
-func heapSort(arr []int) {
 	n := len(arr)
+	heapSortMax(arr, n)
+	fmt.Println("Sorted Array Ascending:-", arr)
+	heapSortMin(arr, n)
+	fmt.Println("Sorted Array Decending:-", arr)
+}
+func heapSortMax(arr []int, n int) {
 	for i := n/2 - 1; i >= 0; i-- {
 		maxHeapify(arr, n, i)
 	}
@@ -18,18 +20,42 @@ func heapSort(arr []int) {
 		maxHeapify(arr, i, 0)
 	}
 }
+func heapSortMin(arr []int, n int) {
+	for i := n/2 - 1; i >= 0; i-- {
+		minHeapify(arr, n, i)
+	}
+	for i := n - 1; i >= 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+		minHeapify(arr, i, 0)
+	}
+}
 func maxHeapify(arr []int, n, i int) {
 	largest := i
 	left := 2*i + 1
 	right := 2*i + 2
-	if left < n && arr[left] > arr[largest] {
+	for left < n && arr[left] > arr[largest] {
 		largest = left
 	}
-	if right < n && arr[right] > arr[largest] {
+	for right < n && arr[right] > arr[largest] {
 		largest = right
 	}
 	if largest != i {
 		arr[i], arr[largest] = arr[largest], arr[i]
 		maxHeapify(arr, n, largest)
+	}
+}
+func minHeapify(arr []int, n, i int) {
+	small := i
+	left := 2*i + 1
+	right := 2*i + 2
+	for left < n && arr[left] < arr[small] {
+		small = left
+	}
+	for right < n && arr[right] < arr[small] {
+		small = right
+	}
+	if small != i {
+		arr[i], arr[small] = arr[small], arr[i]
+		minHeapify(arr, n, small)
 	}
 }
