8f9142240bc49b4cbb00a36d12e2c495f5722e98
akhi9550 <akhilc248@gmail.com>
2024-07-26 11:30:37 +0530

    LeetCode question 100

---
+++ LeetCode/100/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	}
+	if p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return false
+}
+
+func main() {
+	tree1 := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+		Right: &TreeNode{
+			Val: 3,
+		},
+	}
+
+	tree2 := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+		Right: &TreeNode{
+			Val: 3,
+		},
+	}
+
+	if isSameTree(tree1, tree2) {
+		fmt.Println("The trees are the same.")
+	} else {
+		fmt.Println("The trees are not the same.")
+	}
+}
