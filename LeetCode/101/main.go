22cf6cafef3767c077b69c619955cd3f564bf136
akhi9550 <akhilc248@gmail.com>
2024-08-11 19:55:52 +0530

    LeetCode question 101

---
+++ LeetCode/101/main.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return helper(root, root)
+}
+
+func helper(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	return left.Val == right.Val && helper(left.Right, right.Left) && helper(left.Left, right.Right)
+}
+
+func main() {
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 3,
+			},
+			Right: &TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+			},
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+	}
+
+	fmt.Println("Is the tree symmetric?", isSymmetric(root)) // Output: true
+
+	root2 := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+		Right: &TreeNode{
+			Val: 2,
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+	}
+
+	fmt.Println("Is the tree symmetric?", isSymmetric(root2)) // Output: false
+}
