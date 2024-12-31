81f943821f2411180838eaed764a5c6680a00096
akhi9550 <akhilc248@gmail.com>
2024-12-31 14:27:45 +0530

    LeetCode question 21

---
+++ LeetCode/21/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	var res ListNode
+	cur := &res
+	for list1 != nil || list2 != nil {
+		if list1 == nil {
+			cur.Next = list2
+			break
+		}
+		if list2 == nil {
+			cur.Next = list1
+			break
+		}
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			cur = cur.Next
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			cur = cur.Next
+			list2 = list2.Next
+		}
+	}
+	return res.Next
+}
+
+func printList(head *ListNode) {
+	for head != nil {
+		fmt.Printf("%d ", head.Val)
+		head = head.Next
+	}
+	fmt.Println()
+}
+
+func main() {
+	list1 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}
+	list2 := &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}
+	fmt.Println("List 1:")
+	printList(list1)
+	fmt.Println("List 2:")
+	printList(list2)
+	mergedList := mergeTwoLists(list1, list2)
+	fmt.Println("Merged List:")
+	printList(mergedList)
+}
